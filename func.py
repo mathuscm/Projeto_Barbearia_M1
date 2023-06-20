0f771cc5a5a2a95744751fb522f6c4acd6bbff30
Matheus S Cordaro <132008218+mathuscm@users.noreply.github.com>
2023-06-19 22:57:12 -0300

    Funções do programa
    Arquivo de funções utilizada no programa

---
+++ func.py
@@ -0,0 +1,28 @@
+# Aqu pra criar as funções e chamar como biblioteca no main.py
+
+# função para retornar pro sobre-nos ou terminar
+def retornopequeno():
+    print('gostaria de saber algo mais? \n'
+          '1 - Sim\n'
+          '0 - Não\n')
+    resposta = input()
+    if resposta == '1':
+        return
+    elif resposta == '0':
+        print(f'Obrigado por entrar em contato conosco!\n'
+              f'Volte sempre!')
+        exit()
+
+#funcao para receber o dia do agendamento
+def dia_por_extenso(numero):
+    dias = {
+        1: 'Segunda-feira',
+        2: 'Terça-feira',
+        3: 'Quarta-feira',
+        4: 'Quinta-feira',
+        5: 'Sexta-feira',
+        6: 'Sábado',
+        7: 'Domingo'
+    }
+    return dias[numero]
+
